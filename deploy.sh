d23ce92b85d92cab9a48f7c18daf5ac652cc9ba0
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2025-03-29 11:47:46 +0530

    added deployment script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+echo "Deploy all the resources"
+
+kubectl apply -f mongo-secret.yaml
+kubectl apply -f mongodb-deployment.yaml
+kubectl apply -f backend-deployment.yaml
+kubectl apply -f frontend-config.yaml
+kubectl apply -f frontend-deployment.yml
